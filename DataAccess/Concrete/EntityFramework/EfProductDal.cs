7535d8aa93722bfaea6d1701fdd8e949f2e85754
ibrhmusta <ibrhmusta99@gmail.com>
2021-02-13 11:46:49 +0300

    fds

---
+++ DataAccess/Concrete/EntityFramework/EfProductDal.cs
@@ -0,0 +1,36 @@
+﻿using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataAccess.Concrete.EntityFramework
+{
+    class EfProductDal : IProductDal
+    {
+        public void Add(Product product)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(Product product)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Product> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Product> GetAllByCategory(int categoryId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Product product)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
